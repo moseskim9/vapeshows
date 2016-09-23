94e4c5c8def21a8e8886316cac6d27794d583cb8
Moses Kim <moseskim92@yahoo.com>
2016-09-23 15:57:50 -0700

    testing

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -26,12 +26,14 @@ Devise.setup do |config|
   # available as additional gems.
   require 'devise/orm/active_record'
 
-  config.omniauth :facebook, ENV["FB_ID"], ENV["FB_SECRET"],
-      scope: 'email',
-      info_fields: 'email, first_name, last_name',
-      image_size: 'square',  # 50x50, guaranteed ratio
-      secure_image_url: true
+  # config.omniauth :facebook, ENV["FB_ID"], ENV["FB_SECRET"],
+  #     scope: 'email',
+  #     info_fields: 'email, first_name, last_name',
+  #     image_size: 'square',  # 50x50, guaranteed ratio
+  #     secure_image_url: true
 
+  # config.omniauth :facebook, "185705521856637", "8e892e5059501e678abfd813a6766866",
+  #                 callback_url: "https://vapeshows.herokuapp.com/users/auth/facebook/callback"
 
   # ==> Configuration for any authentication mechanism
   # Configure which keys are used when authenticating a user. The default is
--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,4 +1,4 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :facebook, ENV['FACEBOOK_KEY'], ENV['FACEBOOK_SECRET'],
+  provider :facebook, "185705521856637", "8e892e5059501e678abfd813a6766866",
            :scope => 'email,user_birthday,read_stream', :display => 'popup'
 end
