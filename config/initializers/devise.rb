c134e3a4929f075749d33446d1359be8699c1acf
Moses Kim <moseskim92@yahoo.com>
2016-09-23 15:38:45 -0700

    corrected app secret key

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -32,8 +32,8 @@ Devise.setup do |config|
       image_size: 'square',  # 50x50, guaranteed ratio
       secure_image_url: true
 
-  # config.omniauth :facebook, "185705521856637", "866c414d050c32e3f0723635c5b17925",
-  #                 callback_url: "https://vapeshows.herokuapp.com/users/auth/facebook/callback"
+  config.omniauth :facebook, "185705521856637", "8e892e5059501e678abfd813a6766866",
+                  callback_url: "https://vapeshows.herokuapp.com/users/auth/facebook/callback"
 
   # ==> Configuration for any authentication mechanism
   # Configure which keys are used when authenticating a user. The default is
