f3b85cf44f854f54258a23c32aafce324ba6b3ec
Moses Kim <moseskim92@yahoo.com>
2016-09-23 16:01:18 -0700

    Back to normal and lewagon way

--- config/initializers/omniauth.rb
+++
@@ -1,4 +0,0 @@
-Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :facebook, "185705521856637", "8e892e5059501e678abfd813a6766866",
-           :scope => 'email,user_birthday,read_stream', :display => 'popup'
-end
