bc53f6e95d880ee62579a4baaf69af72dea29eb1
Moses Kim <moseskim92@yahoo.com>
2016-09-23 16:32:07 -0700

    a

--- config/routes.rb
+++ config/routes.rb
@@ -3,14 +3,14 @@ Rails.application.routes.draw do
   devise_for :users,
       controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
 
-  devise_scope :user do
-     get 'sign_in', :to => 'devise/sessions#new', :as => :new_user_session
-     delete 'sign_out', :to => 'devise/sessions#destroy', :as => :destroy_user_session
-   end
+  # devise_scope :user do
+  #    get 'sign_in', :to => 'devise/sessions#new', :as => :new_user_session
+  #    delete 'sign_out', :to => 'devise/sessions#destroy', :as => :destroy_user_session
+  #  end
 
   root 'pages#home'
 
-  # resources :users
+  resources :users
 
   resources :events do
 
