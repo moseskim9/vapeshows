8a257aa90ca766fc7feede2d297f6fd0c166be70
Moses Kim <moseskim92@yahoo.com>
2016-09-23 17:07:29 -0700

    reverted back and then also re migreated the user facebook model migration

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -27,10 +27,10 @@ Devise.setup do |config|
   require 'devise/orm/active_record'
 
   config.omniauth :facebook, ENV["FB_ID"], ENV["FB_SECRET"],
-      scope: 'email',
-      info_fields: 'email, first_name, last_name',
-      image_size: 'square',  # 50x50, guaranteed ratio
-      secure_image_url: true
+    scope: 'email',
+    info_fields: 'email, first_name, last_name',
+    image_size: 'square',  # 50x50, guaranteed ratio
+    secure_image_url: true
 
   # config.omniauth :facebook, "185705521856637", "8e892e5059501e678abfd813a6766866",
   #                 callback_url: "https://vapeshows.herokuapp.com/users/auth/facebook/callback"
