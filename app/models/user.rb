c21ee4e5c0b660e7275ceb2d89ea3e0b11bbc906
Moses Kim <moseskim92@yahoo.com>
2016-09-27 11:12:52 -0700

    Also changed the find for google

--- app/models/user.rb
+++ app/models/user.rb
@@ -31,38 +31,55 @@ class User < ApplicationRecord
        return user
      end
 
-     def self.find_for_google_oauth2(access_token, signed_in_resource=nil)
-         data = access_token.info
-         user = User.where(:provider => access_token.provider, :uid => access_token.uid ).first
-         if user
+     # def self.find_for_google_oauth2(access_token, signed_in_resource=nil)
+     #     data = access_token.info
+     #     user = User.where(:provider => access_token.provider, :uid => access_token.uid ).first
+     #     if user
+     #       return user
+     #     else
+     #       registered_user = User.where(:email => access_token.info.email).first
+     #       if registered_user
+     #         return registered_user
+     #       else
+     #         user = User.create(name: data["name"],
+     #           provider:access_token.provider,
+     #           email: data["email"],
+     #           uid: access_token.uid ,
+     #           password: Devise.friendly_token[0,20],
+     #         )
+     #       end
+     #    end
+     # end
+
+     def self.find_for_google_oauth2(auth)
+         data = auth.info
+         if validate_email(auth)
+           user = User.where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+             user.provider = auth.provider
+             user.uid = auth.uid
+             user.email = auth.info.email
+             user.password = Devise.friendly_token[0,20]
+           end
+           user.token = auth.credentials.token
+           user.refresh_token = auth.credentials.refresh_token
+           user.save
            return user
          else
-           registered_user = User.where(:email => access_token.info.email).first
-           if registered_user
-             return registered_user
-           else
-             user = User.create(name: data["name"],
-               provider:access_token.provider,
-               email: data["email"],
-               uid: access_token.uid ,
-               password: Devise.friendly_token[0,20],
-             )
-           end
-        end
-     end
+           return nil
+         end
 
-   def self.from_omniauth(access_token)
-       data = access_token.info
-       user = User.where(:email => data["email"]).first
+   # def self.from_omniauth(access_token)
+   #     data = access_token.info
+   #     user = User.where(:email => data["email"]).first
 
-       # Uncomment the section below if you want users to be created if they don't exist
-       # unless user
-       #     user = User.create(name: data["name"],
-       #        email: data["email"],
-       #        password: Devise.friendly_token[0,20]
-       #     )
-       # end
-       user
-   end
+   #     # Uncomment the section below if you want users to be created if they don't exist
+   #     # unless user
+   #     #     user = User.create(name: data["name"],
+   #     #        email: data["email"],
+   #     #        password: Devise.friendly_token[0,20]
+   #     #     )
+   #     # end
+   #     user
+   # end
 
 end
