c0b7c5dcaa8ce83d94a282da735d9dd45f979dd3
Moses Kim <moseskim92@yahoo.com>
2016-09-27 12:33:45 -0700

    user model saved

--- app/models/user.rb
+++ app/models/user.rb
@@ -27,19 +27,19 @@ class User < ApplicationRecord
        return user
      end
 
-     def self.from_omniauth(access_token)
-         data = access_token.info
-         user = User.where(:email => data["email"]).first
+   def self.from_omniauth(access_token)
+       data = access_token.info
+       user = User.where(:email => data["email"]).first
 
-         # Uncomment the section below if you want users to be created if they don't exist
-         # unless user
-         #     user = User.create(name: data["name"],
-         #        email: data["email"],
-         #        password: Devise.friendly_token[0,20]
-         #     )
-         # end
-         user
-     end
+       # Uncomment the section below if you want users to be created if they don't exist
+       # unless user
+       #     user = User.create(name: data["name"],
+       #        email: data["email"],
+       #        password: Devise.friendly_token[0,20]
+       #     )
+       # end
+       user
+   end
 
 
 end
