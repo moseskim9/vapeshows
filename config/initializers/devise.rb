adc7946d390822eebbb630275f227e8fef2e084f
Moses Kim <moseskim92@yahoo.com>
2016-09-26 22:43:47 -0700

    Fixed the client id by adding the variables in heroku side

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -12,7 +12,7 @@ Devise.setup do |config|
 
   config.omniauth :google_oauth2, ENV['GOOGLE_CLIENT_ID'],
     ENV['GOOGLE_CLIENT_SECRET'],
-    { access_type: "offline",
+    { access_type: "online",
       prompt: "consent",
       select_account: true,
       scope: 'userinfo.email,calendar' }
