02c9e34c8adf7432da945aa8fcd5fd37e481677f
Moses Kim <moseskim92@yahoo.com>
2016-10-04 15:08:20 -0700

    changed the region pictures

--- app/models/event.rb
+++ app/models/event.rb
@@ -10,12 +10,24 @@ class Event < ApplicationRecord
                  "Asia": 5
               }
 
-
   geocoded_by :address
+
   after_validation :geocode, if: :address_changed?
 
   mount_uploader :logo, PhotoUploader
   mount_uploader :primary_image, PhotoUploader
 
+  validates :name, presence: true, length: { minimum: 2 }
+  validates :venue_name, presence: true
+  validates :address, presence: true
+  validates :country, presence: true
+  validates :region, presence: true
+  validates :description, presence: true
+  validates :start_date, presence: true
+  validates :end_date, presence: true
+  validates :start_time, presence: true
+  validates :end_time, presence: true
+  validates :logo, presence: true
+  validates :primary_image, presence: true
 
 end
