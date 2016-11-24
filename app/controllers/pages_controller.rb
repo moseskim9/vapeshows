f2158ae74da8544fc7b93599489f039babca062c
Moses Kim <moseskim92@yahoo.com>
2016-11-24 13:45:28 -0800

    Modifed from vapeshows to small businesses and added heroku

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -5,11 +5,13 @@ class PagesController < ApplicationController
     @reviews = Review.all
 
 
-    @events = Event.where(["start_date > ?", Time.current])
+    @upcoming_events = Event.where(["start_date > ?", Time.current])
     @sorted = @events.sort_by &:start_date
 
     @latest_events = @sorted.first(3)
     @latest_reviews = @reviews.last(3)
+    @latest_users = @users.last(3)
+
   end
 
   def about
